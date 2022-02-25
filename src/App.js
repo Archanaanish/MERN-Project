1f22bab011a3a11519aa3589aeee54284c69f61b
Archanaanish <archanaanish27@gmail.com>
2022-02-25 09:40:58 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import LandingPage  from './screens/LandingPage/LandingPage';
+import {BrowserRouter, Route} from 'react-router-dom';
+import MyNotes from'./screens/MyNotes/MyNotes';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen/RegisterScreen';
+import CreateNote from './screens/CreateNote/CreateNote'
+import SingleNote from './screens/SingleNote/SingleNote';
+import ProfileScreen from "./screens/ProfileScreen/ProfileScreen";
+import { useState } from 'react';
+
+const App = () => {
+
+  const [search, setSearch] = useState("");
+
+ console.log(search);
+
+  return(
+ <BrowserRouter>
+    <Header setSearch={ setSearch} />
+    <main  >
+   <Route path='/' component={LandingPage} exact/>
+   <Route path='/login' component={LoginScreen} exact/>
+   <Route path='/profile' component={ProfileScreen} exact/>
+   <Route path='/register' component={RegisterScreen} exact/>
+   <Route path='/createnote' component={CreateNote} exact/>
+   <Route path='/note/:id' component={SingleNote} exact/>
+   <Route path='/mynotes' component={() => <MyNotes search={search}/>} >
+
+   </Route>
+    </main>
+  
+  <Footer/>
+  </BrowserRouter>
+
+
+
+
+)}
+
 
 export default App;
